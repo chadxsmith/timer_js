c06764afa412d02ff7b8df9457c7620d5dd6f01e
Chad Smith <chadxsmith@ymail.com>
2015-04-30 13:37:31 -0400

    refactored hw

--- timers.js
+++ timers.js
@@ -6,31 +6,37 @@ var reset = document.getElementById('reset')
 var increment;
 
 // Time
-seconds = 0
-
+var seconds = 0
 
 //Event Listeners
 start.addEventListener("click", startTime)
 pause.addEventListener("click", pauseTime)
 reset.addEventListener("click", resetTime)
 
-//Functions
-function startTime(){
-
-      increment = setInterval(function startTime() {  //setInterval is putting a batter in your watch
-          document.getElementById("timer").innerHTML = "Stop Watch:" + seconds;
-          seconds++;
-      }, 1000);
-
+//Functions (to increment the time)
+function incrementTime(){
+    document.getElementById("timer").innerHTML = "Stop Watch:" + seconds;
+    seconds++;
 }
 
 
+
+
+
+//Functions (to increment the time by interval)
+function startTime() {
+         increment = setInterval(incrementTime, 1000);
+}
+
+//Functions  to pause time
 function pauseTime() {
-      clearInterval(increment)  //clearInterval is like pulling out a battery in your watch
+          clearInterval(increment)  //clearInterval is like pulling out a battery in your watch. thus it cancels the repeated action
 }
 
+
+// Functions to reset time
 function resetTime() {
-      seconds = 0
+          seconds = 0
 }
 
 
